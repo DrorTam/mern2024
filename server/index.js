a650c94598e367b7e8e7493a1882732aee2c45fd
Dror Tam <drortm38@gmail.com>
2024-12-10 17:39:43 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import movieRoute from "./routes/movieRoute.js";
+import dotenv from "dotenv";
+import path from 'path';
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/movies", movieRoute);
+
+// use the client app
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+// Render client for any path
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/client/dist/index.html')))
+
+const port = 5555;
+
+mongoose
+  .connect(`${process.env.mongoDBURL}`)
+  .then(() => {
+    console.log("We are connected to database");
+    app.listen(port, () => {
+      console.log(`App is listening to port : ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
