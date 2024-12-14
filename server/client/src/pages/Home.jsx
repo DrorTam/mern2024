4d243b559a26a0f35c92f394fd68f162599b87e3
DrorTam <83964248+DrorTam@users.noreply.github.com>
2024-12-14 05:02:29 +0200

    Update Home.jsx

--- server/client/src/pages/Home.jsx
+++ server/client/src/pages/Home.jsx
@@ -21,11 +21,7 @@ const Home = () => {
     navigate("/");
   };
 
-  const myStyle = {
-    backgroundImage: "url(/cinema93.jpg)",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  };
+  
 
   useEffect(() => {
     setLoading(true);
@@ -42,7 +38,7 @@ const Home = () => {
   }, []);
 
   return (
-    <div style={myStyle} className="p-4">
+    <div className="p-4">
       <button
         className="bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg font-bold border-2 border-black"
         onClick={handleOut}
